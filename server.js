204812e2f3c1b81de8cc33673712aff1fa444f04
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-16 13:48:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/', function (req, res) {
 app.get('/page-one',function (req,res){
     res.send('Article one requested and will be served here');
 });
+
+app.get('/page-2',function (req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'page-2.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
