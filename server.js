6b1abeb939c8ec5f82c9f39a7616d674a00f01f1
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-18 14:56:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,8 +79,8 @@ app.get('/:articleName',function (req,res){
 });
 
 
-app.get('/ui/main.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.css'));
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
