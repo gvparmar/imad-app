b251a09e5173776e672b10eae637c4ce78cb9395
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-18 11:16:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var Articleone ={
+    title: 'Article || GVPARMAR',
+    heading: 'Article-one',
+    date :'feb 23 2018',
+    content:,
+    <p>
+     Q-1   what is microprocessor?
+           Ans:It is a program controlled semi conductor device (IC), which fetches, Decodes and execute instructions.
+    </p>
+    <p>
+     Q-2 what is the function of program counter? A1: - program counter is the 16 bit counter.
+
+            1.   Sequncelly program run.
+            2.   If contain the memory address of the instruction which is next fetched.
+    </p>
+    ,
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,8 +32,8 @@ app.get('/page-one',function (req,res){
     res.send('Article one requested and will be served here');
 });
 
-app.get('/Article-one',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
+app.get('/Articleone',function (req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'Articleone.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
