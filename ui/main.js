cce5e46e4457eaf0abc19c9c80cf0680f5c497a9
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-19 11:22:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,12 @@ console.log('Loaded!');
 var element = document.getElementById('main-text'
 );
 element.innerHTML = 'Avpti RAJKOT';
+var img = document.getElementById('modi');
+var marginLeft =0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight,50);
+};
