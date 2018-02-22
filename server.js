00eb126e95cdb952cc93442dc27d78e46603c983
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-22 12:44:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/test-db', function(req,res){
+    
+});
 
 app.get('/:articleName',function (req,res){
     var articleName = req.params.articleName;
