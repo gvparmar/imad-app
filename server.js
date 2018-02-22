e89c2a4772355ea8db6f878f02de81ced025a26f
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-22 15:55:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,11 +102,32 @@ app.get('/test-db', function(req,res){
 
 
 
+app.get('/articles/:articlename', function (req, res) {
+ // res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+ 
+ pool.query("SELECT * FROM article WHERE title = '"+ req.params.articleName + "'",function(err,result){
+     if (err)
+     {
+        res.status(500).send(err.toString());
+    }
+   else
+   { if(result.rows.length===0) {
+       result.status(404).send('Article not found');
+   }
+   else
+   { var articleData=result.rows[0];
+    res.send(createTemplate(articleData));
+ }
+   }
+   });
 
-app.get('/:articleName',function (req,res){
+  });
+
+
+/*app.get('/:articleName',function (req,res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
-});
+});*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
