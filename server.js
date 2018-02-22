d7402d6ea37252a65a3c126dd09fb23434ec55f0
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-22 16:53:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ return htmlTemplate;
 }
 
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
@@ -98,7 +98,7 @@ app.get('/test-db', function(req,res){
         }
     
 });
-});
+});*/
 
 
 
