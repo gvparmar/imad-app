6071f33b598790a065e8c5358187cda2d82bdb64
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-22 17:25:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get('/articles/:articleName', function (req, res) {
        res.status(404).send('Article not found');
    }
    else
-   { var articleData=result.rows[1];
+   { var articleData=articleName;
     res.send(createTemplate(articleData));
  
    }
