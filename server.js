5c36ed1cd29fdbbbca31279710c15cad48a73a3e
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-23 15:53:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,10 +80,20 @@ var htmlTemplate = `
 return htmlTemplate;
 }
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash (input, salt){
+    var hashed = crypto.pbkdf2sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req, res){
+    var hashedString =hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+});
+
 
 
 var pool = new pool(config);
