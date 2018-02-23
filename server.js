055f943be97c1aa2219572d02a512881f8af6a0c
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-23 17:00:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
 var pool = require('pg').Pool;
-
+var bodyParser = require('body-parser');
 var config ={
     user:'parmarsir71',
     database:'parmarsir71',
@@ -16,6 +16,7 @@ var config ={
 var app = express();
 app.use(morgan('combined'));
 
+app.user(bodyParser.json());
 var articles = {
     
     'articleone' : {
@@ -94,9 +95,25 @@ app.get('/hash/:input', function(req, res){
     res.send(hashedString);
 });
 
-
+app.post('/creat-user', function(req,res){
+    var username =req.body.username;
+    var password = req.body.passwoord;
+    
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    pool.query('INSERT INTO "user"(username,password) VALUE ($1,$2)',[username, dbString],function(err, result){
+    if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send('user sucessfully created:',+username);
+        }    
+    });
+});
 
 var pool = new pool(config);
+
+
 app.get('/test-db', function(req,res){
     
     pool.query('SELECT * FROM test', function(err,result){
