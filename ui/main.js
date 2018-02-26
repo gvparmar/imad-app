a6fb3b07a7663e8c7463bae5096b72258dea8344
gvparmar <36467905+gvparmar@users.noreply.github.com>
2018-02-26 16:34:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,22 @@
-console.log('Loaded!');
-/*var element = document.getElementById('main-text'
-);
-element.innerHTML = 'Avpti RAJKOT';*/
-
-
-var img = document.getElementById('madi');
-var marginLeft =0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
+var submit= document.getElementById('submit_btn');
+submit.onclick=function (){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (request.redystate===XMLHttpRequest.Done){
+            if(request.status===200){
+                alert('Login Sucessfully');
+            }else if(request.status===403){
+                alert('Use Username/paswword incorrect');
+            }else if(request.status===500){
+                alert('Some thing went rong on server');
+            }
+        }
+    };
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://parmarsir71@ssh.imad.hasura-app.io',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username,password: password}));
 };
